a408bc16e88bbbc91b88c36fcaff313c2b4683a1
Matthias Leuffen <matthes@leuffen.de>
2024-02-16 14:01:42 +0100

    sync

--- src/Output/Out.php
+++ src/Output/Out.php
@@ -18,11 +18,17 @@ class Out
     }
     
     
-    public static function TextDanger(string $text) {
-        return "\033[31m$text\033[0m";
+    public static function TextDanger(string $text, bool $return = false) :?string {
+        $text =  "\033[31m$text\033[0m";
+        if ($return)
+            return $text;
+        echo $text;
     }
-    public static function TextWarning(string $text) {
-        return "\033[33m$text\033[0m";
+    public static function TextWarning(string $text, bool $return = false) : ?string {
+        $text =  "\033[33m$text\033[0m";
+        if ($return)
+            return $text;
+        echo $text;
     }
     
 }
